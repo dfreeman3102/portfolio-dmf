71f643ecee6757f667a1718bf35b46a18dba7f7f
dfreeman3102 <davidfreeman3102@gmail.com>
2024-06-22 14:27:05 -0400

    added footer with links to social media

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/dfreeman3102">GitHub</a>
+      <a href="https://www.linkedin.com/in/david-freeman-536486294/">
+        LinkedIn
+      </a>
+      <a href="https://www.facebook.com/profile.php?id=100006023509245">
+        Facebook
+      </a>
+      <p>© 2021</p>
+    </footer>
+  );
+}
